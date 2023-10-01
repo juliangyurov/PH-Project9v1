61b0f39dc3e961abede019a99034d8466b5b5e53
Yulian Gyurov <julian.gyuroff@gmail.com>
2023-10-01 21:19:27 +0300

    Add second UITabBarController - not in AppDelegate but in SceneDelegate.

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -13,17 +13,33 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        }else{
+            //urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            //urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
         if let url = URL(string: urlString){
             if let data = try? Data(contentsOf: url){
                 //Data fetched OK
                 //print("data.count=\(data.count)")
                 parse(json: data)
-                
+                return
             }
         }
+        showError()
      }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; Check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        
+    }
 
     func parse(json: Data){
         let decoder = JSONDecoder()
